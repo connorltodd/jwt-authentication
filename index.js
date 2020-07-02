24233689a64a476461d3df4a92d06711b8f910b4
DEV <connortodd0304@gmail.com>
2020-07-02 17:23:06 +0100

    jwt authentication implemented

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const connection = require('./config');
+const authRouter = require('./routes/auth');
+const port = 5000;
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Successfully connected to the database');
+});
+
+app.use(express.json());
+app.use('/auth', authRouter);
+
+
+app.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`App is listening at ${port}`)
+});
